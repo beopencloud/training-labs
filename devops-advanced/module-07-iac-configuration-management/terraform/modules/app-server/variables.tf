ed9efb6bf4eeea9d20e7276a5c37aa0038a87f04
Maïmouna Diagne <mayadiagne@MacBook-Pro-de-Maimouna.local>
2026-07-06 10:36:00 +0000

    reload devops labs

--- devops-advanced/module-07-iac-configuration-management/terraform/modules/app-server/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "environment"  { type = string }
-variable "app_name"     { type = string; default = "todo-api" }
-variable "app_version"  { type = string; default = "1.0.0" }
-variable "app_port"     { type = number; default = 5000 }
-variable "replicas"     { type = number; default = 2 }
-variable "log_level"    { type = string; default = "INFO" }
-variable "output_dir"   { type = string; default = "./output" }
-variable "docker_image" { type = string }
--- devops-advanced/module-07-iac-configuration-management/terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "docker_image" {
-  description = "Image Docker à déployer / Docker image to deploy"
-  type        = string
-  default     = "votre_username/todo-api"
-}
